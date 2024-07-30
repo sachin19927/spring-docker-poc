6e5ab37cd1f7c2ff4ba800a657acdb7d6976d756
SACHIN HS <sachin19927@gmail.com>
2024-07-31 01:24:54 +0530

    V3 zipkin and Promethus and Grafana

--- src/main/java/com/spring/docker/poc/controller/SampleController.java
+++ src/main/java/com/spring/docker/poc/controller/SampleController.java
@@ -3,12 +3,16 @@ package com.spring.docker.poc.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class SampleController {
 
 	@GetMapping("/version")
 	public String testMethod()
 	{
+		log.info("calling Version Method");
 		return "v3";
 	}
 }
